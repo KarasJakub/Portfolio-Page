803fadc0dfa71035f504b733e5568f27b8bdd859
Jakub Karaś <92886013+KarasJakub@users.noreply.github.com>
2022-10-31 19:47:47 +0100

    Created About Section
    * added components and text (only mobile)
    
    * added image
    
    * added icons
    
    * added strong to text and changed margines, paddings etc...

---
+++ src/components/About/About.tsx
@@ -0,0 +1,69 @@
+import { useImagesContext } from "context/ImagesContext"
+import { getImage } from "gatsby-plugin-image"
+import React from "react"
+import * as S from "./About.styled"
+import GatsbyIcon from "assets/icons/Gatsby.svg"
+import ReactIcon from "assets/icons/React.svg"
+import TypeScriptIcon from "assets/icons/Typescript.svg"
+import WebpackIcon from "assets/icons/Webpack.svg"
+import NodeIcon from "assets/icons/Node.svg"
+import JavaScriptIcon from "assets/icons/Javascript.svg"
+
+const About = () => {
+  const { Personal_Image } = useImagesContext()
+
+  return (
+    <S.AboutWrapper>
+      <S.TextWrapper>
+        <S.SectionTitle>About me</S.SectionTitle>
+        <S.SectionParagraph>
+          I&apos;m 19 years Junior Front-End Developer from{" "}
+          <strong>Poland</strong>. My work is my passion, I{" "}
+          <strong>enjoy</strong> building websites and developing my skillset
+          because, as I said I&apos;m really into the idea of{" "}
+          <strong>creating something out of nothing</strong> on the web. I
+          started back in 2020 with fundamentals of web development and started
+          creating my first&apos;s websites. At this point,{" "}
+          <strong>my main focus</strong> is building modern and good-looking
+          websites along with new technologies, especially my favorite –{" "}
+          <strong>React.js</strong>.
+        </S.SectionParagraph>
+        <S.SectionParagraph>
+          I&apos;m in 4th-grade Secondary School of Information Technology
+        </S.SectionParagraph>
+        <S.SectionParagraph>
+          What technologies am I working with?
+        </S.SectionParagraph>
+        <S.Icons>
+          <S.IconBar>
+            <img src={GatsbyIcon} alt="Gatsby Icon" />
+            <p>Gatsby.js</p>
+          </S.IconBar>
+          <S.IconBar>
+            <img src={ReactIcon} alt="React Icon" />
+            <p>React.js</p>
+          </S.IconBar>
+          <S.IconBar>
+            <img src={TypeScriptIcon} alt="Typescript Icon" />
+            <p>Typescript</p>
+          </S.IconBar>
+          <S.IconBar>
+            <img src={WebpackIcon} alt="Webpack Icon" />
+            <p>Webpack</p>
+          </S.IconBar>
+          <S.IconBar>
+            <img src={NodeIcon} alt="Node.js Icon" />
+            <p>Node.js</p>
+          </S.IconBar>
+          <S.IconBar>
+            <img src={JavaScriptIcon} alt="Javascript Icon" />
+            <p>Javascript</p>
+          </S.IconBar>
+        </S.Icons>
+      </S.TextWrapper>
+      <S.Image image={getImage(Personal_Image)} alt="Photo of author" />
+    </S.AboutWrapper>
+  )
+}
+
+export default About
