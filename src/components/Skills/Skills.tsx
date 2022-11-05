becbee93f6ea7506ea85de3b282862e2175a835e
Jakub Karaś <jakub.karas@onet.pl>
2022-11-05 23:47:33 +0100

    added selection by clicking

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -5,16 +5,49 @@ import Languages_Icon from "assets/icons/Languages_Icon.svg"
 import Programming_Icon from "assets/icons/Programming_Icon.svg"
 import Design_Icon from "assets/icons/Design_Icon.svg"
 
+const steps = [
+  {
+    name: "Languages",
+    description:
+      "I understand English very well. Learning from English documentation, and video tutorials isn’t a challenge for me. I’m developing my English skillset every day, now I’m focused on improving my fluency in speaking. Polish is my native language."
+  },
+  {
+    name: "Programming skills",
+    description:
+      "After I learned the basics of creating pages, I started learning new technologies: webpack, node.js, react.js, etc… They were totally game changers, projects I’ve built with them taught (and still are teaching :P) me so much about problem-solving, humility and specific mindset in programming. I think these experiences are making me a better developer every day. Actually, my leading technology is React (especially Gatsby)."
+  },
+  {
+    name: "Design skills",
+    description:
+      "I adore pretty websites, but design just isn’t my cup of tea. I prefer coding websites rather than designing them :P. I designed several websites in my career, but I haven’t made any UI/UX course, I like the idea of Just-In-Time-Learning :P. For occasionally designing I use Figma"
+  }
+]
+
 const Skills = () => {
+  const [activeStep, setActiveStep] = React.useState(0)
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveStep(prev => (prev + 1) % steps.length)
+    }, 8000)
+
+    return () => clearInterval(interval)
+  }, [])
   return (
     <S.RootWrapper>
       <S.MobilesWrapper>
         <S.SectionTitle>Languages</S.SectionTitle>
         <S.Content>
-          <SkillsCard title="Languages" image={Languages_Icon} />
-          <SkillsCard title="Programming" image={Programming_Icon} />
-          <SkillsCard title="Design" image={Design_Icon} />
-          <S.Text></S.Text>
+          {steps.map((step, index) => (
+            <SkillsCard
+              title={step.name}
+              image={Languages_Icon}
+              key={step.name}
+              active={index === activeStep}
+              onClick={() => setActiveStep(index)}
+            />
+          ))}
+          <S.Text>{steps[activeStep].description}</S.Text>
         </S.Content>
       </S.MobilesWrapper>
     </S.RootWrapper>
--- src/components/Skills/SkillsCard/SkillsCard.tsx
+++ src/components/Skills/SkillsCard/SkillsCard.tsx
@@ -4,11 +4,12 @@ import * as S from "./SkillsCard.styled"
 export interface Props {
   title: string
   image: string
+  onClick?: () => void
 }
 
 const SkillsCard = (props: Props) => {
   return (
-    <S.SkillCardWrapper>
+    <S.SkillCardWrapper onClick={props.onClick}>
       <S.Title>{props.title}</S.Title>
       <S.Icon src={props.image} alt="Language Icon" />
     </S.SkillCardWrapper>
