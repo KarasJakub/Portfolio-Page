da757caed4eca6ddbb178623bda152cb4ad5dac4
Jakub Karaś <jakub.karas@onet.pl>
2022-11-13 18:10:18 +0100

    added SEO Component to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,17 @@ import Skills from "components/Skills/Skills"
 import Sidebar from "components/SideBar/Sidebar"
 import Footer from "components/Footer/Footer"
 import Contact from "components/Contact/Contact"
+import SEO from "components/SEO/SEO"
 
 export default function Home() {
   return (
     <>
+      <SEO title="Jakub Karaś - Front End Developer Portfolio">
+        I&apos;m Junior Front-End Developer from Poland. My work is my passion,
+        I enjoy building websites and developing my skillset because, as I said
+        I&apos;m really into the idea of creating something out of nothing on
+        the web.
+      </SEO>
       <Sidebar />
       <GradientWrapper>
         <>
