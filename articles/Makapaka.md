9fae689bea8025cedc31d35a784b0e7ee538a328
Jakub Karaś <jakub.karas@onet.pl>
2022-11-23 20:54:24 +0100

    fixed doubling templates

--- articles/Makapaka.md
+++ articles/Makapaka.md
@@ -1,12 +1,15 @@
 ---
-title: "Makapaka"
-description: "Makapaka"
-firstTechnology: "Makapaka"
-secondTechnology: "Makapaka"
-slug: "makapaka"
+title: "Lorem ipsum"
+description: "Lorem ipsum"
+firstTechnology: "Lorem ipsum"
+secondTechnology: "Lorem ipsum"
+slug: "Lorem ipsum"
 photo: ../src/assets/images/API.png
-alt: "Makapaka"
+alt: "Lorem ipsum"
 ---
 
-<p>lorem ipsum dolor sit wqnbyfuwfygfufhjfhajsfhajfhjfhfjahfjhsfkhafjhjnxkcmvsovmiewjofwyeuwerh
+<p>rticle evident arrived express highest men did boy. Mistress sensible entirely am so. Quick can manor smart money hopes worth too. Comfort produce husband boy her had hearing. Law others theirs passed but wishes. You day real less till dear read. Considered use dispatched melancholy sympathize discretion led. Oh feel if up to till like.
+
+He unaffected sympathize discovered at no am conviction principles. Girl ham very how yet hill four show. Meet lain on he only size. Branched learning so subjects mistress do appetite jennings be in. Esteems up lasting no village morning do offices. Settled wishing ability musical may another set age. Diminution my apartments he attachment is entreaties announcing estimating. And total least her two whose great has which. Neat pain form eat sent sex good week. Led instrument sentiments she simplicity.
+
 </p>
--- src/templates/CaseStudyTemplate.tsx
+++ src/templates/CaseStudyTemplate.tsx
@@ -2,78 +2,84 @@ import React from "react"
 import * as S from "./CaseStudyTemplate.styled"
 import Navigation from "components/Navigation/Navigation"
 import Footer from "components/Footer/Footer"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import { GatsbyImage, getImage, IGatsbyImageData } from "gatsby-plugin-image"
 import SEO from "components/SEO/SEO"
 
-const CaseStudyTemplate = () => {
-  const {
-    allMarkdownRemark: { edges }
-  } = useStaticQuery(graphql`
-    query TemplateQuery {
-      allMarkdownRemark {
-        edges {
-          node {
-            id
-            html
-            frontmatter {
-              slug
-              title
-              description
-              firstTechnology
-              secondTechnology
-              alt
-              photo {
-                childImageSharp {
-                  gatsbyImageData
-                }
-              }
-            }
-          }
-        }
+interface Props {
+  data: {
+    markdownRemark: {
+      html: string
+      frontmatter: {
+        description: string
+        title: string
+        slug: string
+        photo: IGatsbyImageData
       }
     }
-  `)
+  }
+}
+
+const CaseStudyTemplate = ({ data }: Props) => {
   return (
     <>
-      {edges.map(({ node: { frontmatter } }: any) => (
-        <SEO title={frontmatter.title} key={frontmatter.title}>
-          {frontmatter.description}
-        </SEO>
-      ))}
+      <SEO
+        title={data.markdownRemark.frontmatter.title}
+        key={data.markdownRemark.frontmatter.title}
+      >
+        {data.markdownRemark.frontmatter.description}
+      </SEO>
       <Navigation />
       <S.MainContainer>
-        {edges.map(({ node: { frontmatter } }: any) => (
-          <>
-            <S.TopContainer>
-              <S.TitleContainer>
-                <S.Title>{frontmatter.title}</S.Title>
-                <S.TitleParagraph>{frontmatter.description}</S.TitleParagraph>
-              </S.TitleContainer>
-              <S.ImageWrapper>
-                <GatsbyImage
-                  image={
-                    getImage(
-                      frontmatter.photo.childImageSharp.gatsbyImageData
-                    ) as IGatsbyImageData
-                  }
-                  alt="Image"
-                />
-              </S.ImageWrapper>
-            </S.TopContainer>
-          </>
-        ))}
-        {edges.map(({ node: { html } }: any) => (
-          <>
-            <S.ContentContainer
-              dangerouslySetInnerHTML={{ __html: html }}
-            ></S.ContentContainer>
-          </>
-        ))}
+        <>
+          <S.TopContainer>
+            <S.TitleContainer>
+              <S.Title>{data.markdownRemark.frontmatter.title}</S.Title>
+              <S.TitleParagraph>
+                {data.markdownRemark.frontmatter.description}
+              </S.TitleParagraph>
+            </S.TitleContainer>
+            <S.ImageWrapper>
+              <GatsbyImage
+                image={
+                  getImage(
+                    data.markdownRemark.frontmatter.photo
+                  ) as IGatsbyImageData
+                }
+                alt="Image"
+              />
+            </S.ImageWrapper>
+          </S.TopContainer>
+        </>
+        <>
+          <S.ContentContainer
+            dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+          ></S.ContentContainer>
+        </>
       </S.MainContainer>
       <Footer />
     </>
   )
 }
 
+export const query = graphql`
+  query ArticleDataQuery($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        slug
+        title
+        description
+        firstTechnology
+        secondTechnology
+        alt
+        photo {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`
 export default CaseStudyTemplate
